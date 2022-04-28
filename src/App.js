396d053fa3ec1aa2b9f04aeab9c4ce6eac1a6d9f
Andrew Efurhievwe <andyriles22@gmail.com>
2022-04-28 17:00:11 +0100

    converted to pwa

--- src/App.js
+++ src/App.js
@@ -29,12 +29,12 @@ function App() {
   return (
     <div className="grid place-items-center">
       <div className="w-10/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-6/12">
-        <h1 className="text-3xl font-semibold text-blue-400">
+        <h1 className="text-3xl font-semibold text-blue-400  text-center">
           Select a Bank Provider
         </h1>
         <form onSubmit={handleSubmit} className="mt-6">
           <select
-            className="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline cursor-pointer "
+            className="block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-6 rounded shadow leading-tight focus:outline-none focus:shadow-outline cursor-pointer "
             value={selectedBank}
             onChange={(e) => setSelectedBank(e.target.value)}
           >
@@ -46,14 +46,16 @@ function App() {
           </select>
           <button
             type="submit"
-            className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none"
+            className="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none "
           >
             Reveal Code
           </button>
         </form>
         {displayedCode && (
-          <div>
-            <p className="mt-2 font-bold text-lg">Dial {displayedCode}</p>
+          <div className="flex justify-center text-center">
+            <p className="mt-2 font-bold text-lg">
+              Dial {displayedCode} to check your account balance
+            </p>
           </div>
         )}
       </div>
